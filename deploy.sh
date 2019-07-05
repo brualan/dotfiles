44b9b012b22246ff94781f0213b873801f5c6efd
Alexandr Bruyako <aabruyako@gmail.com>
2019-07-05 13:43:59 +0300

    add fzf installation to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ ln -s $(pwd)/vimrc $HOME/.vimrc
 
 rm $HOME/.tmux.conf
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
